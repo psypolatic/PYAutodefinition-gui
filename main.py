dd42c7883847bd6fe2f1ea10fed6becdc73b0585
Jose Martinez Lopez <98757739+psypolatic@users.noreply.github.com>
2022-10-12 15:10:02 +0000

    figuired out definition output

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ def main():
     gui.title("Quick Definitions")
     gui.geometry("500x500")
     gui['bg']='brown'
-    test_label = Label(gui, text = 'Test input', font=('calibre',10, 'bold'))
+    test_label = Label(gui, text = 'Enter Word List', font=('calibre',10, 'bold'))
 
 
     def submit():
         inputValue=text.get("1.0","end-1c")
-        print(inputValue.split("\n"))
+        words = inputValue.split("\n")
+        print(words)
+        output.insert(INSERT,f"you typed {len(words)} words ")
+
+    def outputdefinition(definition):
+        output.insert(INSERT,definition)
+        #new lines
+        output.insert(INSERT, "\n")
 
     
     sub_btn= Button(gui,text = 'Submit', command = submit)
     text = Text(gui,height=8, width=40)
+    outlabel = Label(gui, text = 'Definitions')
+    output = Text(gui,height = 10, width = 50)
 
     scroll = Scrollbar(gui)
     text.configure(yscrollcommand=scroll.set)
@@ -28,6 +37,8 @@ def main():
     test_label.grid(row=0,column=0)
     text.grid(row=4,column=0,pady=10, padx=10)
     sub_btn.grid(row=4,column=1)
+    outlabel.grid(row = 5, column = 0)
+    output.grid(row=6,column = 0,pady=10, padx=1)
     
 
 
@@ -38,4 +49,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
